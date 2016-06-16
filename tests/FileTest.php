2e69cb783bf274081d98ca2e4606ae634a578c78
Exin Coda <ggaao1994@gmail.com>
2016-06-16 16:20:07 +0800

    tests

---
+++ tests/FileTest.php
@@ -0,0 +1,30 @@
+<?php
+
+class FileTest extends TestCase {
+	/**
+	 * A basic test example.
+	 *
+	 * @return void
+	 */
+	public function test() {
+		// set
+		$cases = [
+			[
+				"fileId" => "",
+				"section" => "",
+				"data" => [],
+				"res_code" => 1,
+			],
+		];
+		foreach ($cases as $case) {
+			$fileId = $case["fileId"];
+			$section = $case["section"];
+			$this->json('post', "file/$fileId/$section", $case["data"])->seeJson([
+				"res_code" => $case["res_code"],
+			]);
+		}
+
+		// get
+
+	}
+}
---
+++ tests/LoginTest.php
@@ -0,0 +1,44 @@
+<?php
+
+class LoginTest extends TestCase {
+	/**
+	 * A basic test example.
+	 *
+	 * @return void
+	 */
+
+	public function test() {
+		$cases = [
+			[
+				"data" => null,
+				"res_code" => 1,
+			],
+			[
+				"data" => ["password" => ""],
+				"res_code" => 1,
+			],
+			[
+				"data" => ["id" => ""],
+				"res_code" => 1,
+			],
+			[
+				"data" => ["id" => "", "password" => ""],
+				"res_code" => 1,
+			],
+			[
+				"data" => ["id" => "10132510349", "password" => "123456"],
+				"res_code" => 1,
+			],
+			[
+				"data" => ["id" => "10132510349", "password" => "29137X"],
+				"res_code" => 0,
+			],
+		];
+		foreach ($cases as $case) {
+			$this->json('post', 'login', $case["data"])->seeJson([
+				"res_code" => $case["res_code"],
+			]);
+		}
+	}
+
+}
